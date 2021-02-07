97961b9c1b6bef6692d0aa072cfcd6e986e689ab
cesnakas <10352420+cesnakas@users.noreply.github.com>
2021-02-07 22:04:49 +0400

    1.0.21

--- security-systems/index.php
+++ security-systems/index.php
@@ -96,9 +96,253 @@ $APPLICATION->SetTitle("Системы безопасности");
 
     </section>
 
-    <section class="mb-5" style="background-color: var(--bs-cyan)">
+    <section class="mb-5 py-5" style="background-color: var(--bs-cyan)">
         <div class="container">
-            ji
+            <h3>Установка функциональных систем видеонаблюдения под ключ</h3>
+
+            <div class="row row-cols-2 row-cols-lg-3 py-5 gy-4 w-50 text-center">
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_ip-video.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">IP видеонаблюдение</figcaption>
+                    </figure>
+                </div>
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_video.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">Видеонаблюдение через интернет</figcaption>
+                    </figure>
+                </div>
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_mobile.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">Беспроводное видеонаблюдение</figcaption>
+                    </figure>
+                </div>
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_street.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">Уличное видеонаблюдение</figcaption>
+                    </figure>
+                </div>
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_auto.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">Автономная система видеонаблюдения</figcaption>
+                    </figure>
+                </div>
+
+                <div class="col -lg-2">
+                    <figure class="figure">
+                        <img
+                            class="figure-img img-fluid"
+                            src="/local/templates/main/img/demo/02_video-space.png"
+                            alt="..."
+                            style="max-width: 105px;"
+                        />
+                        <figcaption class="figure-caption">Видеонаблюдение на расстоянии</figcaption>
+                    </figure>
+                </div>
+
+            </div>
+
+            <p>Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. Я совсем один и блаженствую в здешнем краю, словно созданном для таких, как я. Я так счастлив, мой друг, так упоен ощущением покоя, что искусство мое страдает от этого. Ни одного штриха не мог бы я сделать, а никогда не был таким большим художником, как в эти минуты. Когда от милой моей долины поднимается пар и полдневное солнце стоит над непроницаемой чащей темного леса и лишь редкий луч.</p>
+        </div>
+    </section>
+
+    <section class="mb-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h3 class="mb-3">Сервис Starthelp для видеонаблюдения</h3>
+                    <ul class="list-group list-group-flush mb-3">
+                        <li class="list-group-item">Доставка по Москве и МО Бесплатно</li>
+                        <li class="list-group-item">Выезд инженера Бесплатно </li>
+                        <li class="list-group-item">Настройка комплекта включая удаленный просмотр Бесплатно</li>
+                        <li class="list-group-item">Первичная проверка на брак Бесплатно</li>
+                        <li class="list-group-item">Пакет документов от “ИонДом” Бесплатно</li>
+                    </ul>
+                    <button class="btn btn-primary px-5 py-2">Узнать подробнее</button>
+                </div>
+                <div class="col-lg-6">
+                    <img
+                        class="img-fluid"
+                        src="/local/templates/main/img/demo/starthelp.png"
+                        alt="..."
+                        style="max-height: 338px;"
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="py-4" style="background-color: var(--bs-cyan-2)">
+        <div class="container">
+            <div class="w-50">
+                <p class="mb-0 text-primary">Сервис Starthelp можно получить в подарок! С готовым комплектом видеонаблюдения для самостоятельной установки в <span>Нашем магазине</span></p>
+            </div>
+        </div>
+    </section>
+
+    <section class="py-5" style="background-color: var(--bs-cyan)">
+        <div class="container">
+
+            <h3 class="mb-4 lh-base">
+                Рассчитайте стоимость установки
+                <br>
+                видеонаблюдения под ключ в <span class="text-muted text-decoration-underline">Москве и МО</span>
+            </h3>
+
+            <div class="card mb-5 w-75 border-0 shadow">
+                <div class="p-5">
+                    <h5 class="mb-3">Сервисы</h5>
+
+                </div>
+                <hr>
+                <div class="p-5">
+                    <h5 class="mb-3">Особенности</h5>
+
+                </div>
+                <hr>
+                <div class="p-5">
+                    <h5 class="mb-3">Камеры</h5>
+
+                </div>
+            </div>
+
+            <div class="row row-cols-lg-3 w-75">
+
+                <div class="col">
+                    <div class="fs-6 mb-3">Комплект Стандарт на 18 камер</div>
+                    <div class="card mb-4 border-0 shadow">
+                        <img src="/local/templates/main/img/demo/bg-form.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: var(--bs-green);">Состав системы</h5>
+                        </div>
+                        <ul class="_list-group list-group-flush">
+                            <li class="_list-group-item">Детектор движения</li>
+                            <li class="_list-group-item">Удаленный просмотр</li>
+                            <li class="_list-group-item">Запись звука</li>
+                            <li class="_list-group-item">Умная ИК-подсветка</li>
+                            <li class="_list-group-item">Ночная сьемка</li>
+                            <li class="_list-group-item">Уличное</li>
+                        </ul>
+                        <hr class="m-0">
+                        <div class="card-body">
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between">
+                                <div>Комплект</div>
+                                <div>19 000 ₽</div>
+                            </div>
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between" style="color: var(--bs-orange);">
+                                <small>Сервис</small>
+                                <small>9 999 ₽</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-grid gap-2 d-lg-flex">
+                        <button class="btn btn-primary px-4" type="button">Приобрести</button>
+                        <button class="btn btn-link px-4 text-muted" type="button">Подробнее</button>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="fs-6 mb-3">Комплект Стандарт на 18 камер</div>
+                    <div class="card mb-4 border-0 shadow">
+                        <img src="/local/templates/main/img/demo/bg-form.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: var(--bs-green);">Состав системы</h5>
+                        </div>
+                        <ul class="_list-group list-group-flush">
+                            <li class="_list-group-item">Детектор движения</li>
+                            <li class="_list-group-item">Удаленный просмотр</li>
+                            <li class="_list-group-item">Запись звука</li>
+                            <li class="_list-group-item">Умная ИК-подсветка</li>
+                            <li class="_list-group-item">Ночная сьемка</li>
+                            <li class="_list-group-item">Уличное</li>
+                        </ul>
+                        <hr class="m-0">
+                        <div class="card-body">
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between">
+                                <div>Комплект</div>
+                                <div>19 000 ₽</div>
+                            </div>
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between" style="color: var(--bs-orange);">
+                                <small>Сервис</small>
+                                <small>9 999 ₽</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-grid gap-2 d-lg-flex">
+                        <button class="btn btn-primary px-4" type="button">Приобрести</button>
+                        <button class="btn btn-link px-4 text-muted" type="button">Подробнее</button>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="fs-6 mb-3">Комплект Стандарт на 18 камер</div>
+                    <div class="card mb-4 border-0 shadow">
+                        <img src="/local/templates/main/img/demo/bg-form.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: var(--bs-green);">Состав системы</h5>
+                        </div>
+                        <ul class="_list-group list-group-flush">
+                            <li class="_list-group-item">Детектор движения</li>
+                            <li class="_list-group-item">Удаленный просмотр</li>
+                            <li class="_list-group-item">Запись звука</li>
+                            <li class="_list-group-item">Умная ИК-подсветка</li>
+                            <li class="_list-group-item">Ночная сьемка</li>
+                            <li class="_list-group-item">Уличное</li>
+                        </ul>
+                        <hr class="m-0">
+                        <div class="card-body">
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between">
+                                <div>Комплект</div>
+                                <div>19 000 ₽</div>
+                            </div>
+                            <div class="d-grid gap-2 d-lg-flex justify-content-between" style="color: var(--bs-orange);">
+                                <small>Сервис</small>
+                                <small>9 999 ₽</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-grid gap-2 d-lg-flex">
+                        <button class="btn btn-primary px-4" type="button">Приобрести</button>
+                        <button class="btn btn-link px-4 text-muted" type="button">Подробнее</button>
+                    </div>
+                </div>
+                
+            </div>
+
         </div>
     </section>
 
