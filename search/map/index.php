35fec3e0e543b00851fa8fe67e7216f1e4924580
cesnakas <10352420+cesnakas@users.noreply.github.com>
2021-03-01 14:33:04 +0400

    1.4.45

--- search/map/index.php
+++ search/map/index.php
@@ -1,17 +1,24 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
 $APPLICATION->SetTitle("Карта сайта");
+?>
 
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", array(
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"SET_TITLE" => "Y",
-	"LEVEL" => "4",
-	"COL_NUM" => "2",
-	"SHOW_DESCRIPTION" => "Y"
-	),
-	false
-);
+<div class="container">
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:main.map",
+        ".default",
+        array(
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "36000000",
+            "SET_TITLE" => "Y",
+            "LEVEL" => "4",
+            "COL_NUM" => "2",
+            "SHOW_DESCRIPTION" => "Y"
+        ),
+        false
+    );?>
+</div>
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
